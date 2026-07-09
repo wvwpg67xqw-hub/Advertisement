814a228e9e1af9a800b068deb97df4979fdfa086
wvwpg67xqw-hub <wvwpg67xqw@privaterelay.appleid.com>
2026-07-09 01:50:27 -0400

    Create postgres.js

---
+++ postgres.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import pg from "pg";
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export default pool;
